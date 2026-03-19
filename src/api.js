8a814c45748faf4a13b22e7198a6e8967718c58b
Geo Devassy <gdevassy17@gmail.com>
2026-03-19 15:08:23 +0530

    fix: correct double /api path bug in api.js baseURL

--- src/api.js
+++ src/api.js
@@ -1,7 +1,12 @@
 import axios from "axios";
 
+const BASE_URL =
+  process.env.REACT_APP_API_URL
+    ? process.env.REACT_APP_API_URL.replace(/\/api$/, "")
+    : "https://boxly-backend-xr97.onrender.com";
+
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL
+  baseURL: BASE_URL
 });
 
-export default API;
+export default API;
